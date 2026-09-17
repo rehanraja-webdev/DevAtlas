80a59fa549b145e2db8f899b66bba155b544be37
Rehan Raja <rehanraja.dev@gmail.com>
2026-09-17 13:37:36 +0530

    Add authentication controller with Register, Login, and Protected endpoints

---
+++ server/src/controller/auth.controller.js
@@ -0,0 +1,53 @@
+import { loginUser, me, registerUser } from "../services/auth.service.js";
+
+const Register = async (req, res) => {
+  try {
+    const user = await registerUser(req.body);
+
+    return res.status(201).json({
+      success: true,
+      message: "User Registered!",
+      data: {
+        user,
+      },
+    });
+  } catch (error) {
+    res.status(401).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+const Login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const result = await loginUser(email, password, req);
+
+    res.status(200).json({
+      success: true,
+      message: "Login Successful!",
+      data: {
+        result,
+      },
+    });
+  } catch (error) {
+    res.status(401).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+const Protected = async (req, res) => {
+  const user = await me(req.user);
+
+  return res.status(200).json({
+    success: true,
+    message: "User data fetched!",
+    data: { user },
+  });
+};
+
+export default { Register, Login, Protected };
