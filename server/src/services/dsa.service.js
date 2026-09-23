c10572bdc163b2b11f96c201d32bead4bb2e288b
Rehan Raja <rehanraja.dev@gmail.com>
2026-09-23 23:59:39 +0530

    Add updateUserDSAProgress functionality to track user progress on DSA problems

--- server/src/services/dsa.service.js
+++ server/src/services/dsa.service.js
@@ -1,7 +1,40 @@
 import DSAProblem from "../models/DSAProblem.js";
+import DSAProgress from "../models/DSAProgress.js";
 
 export const getAllProblems = async () => {
   const problems = (await DSAProblem.find()).toSorted({ createdAt: -1 });
 
   return problems;
+};
+
+export const updateUserDSAProgress = async (userId, problemId, status) => {
+  const problem = await DSAProblem.findById(problemId);
+
+  if (!problem) {
+    throw new Error("Problem not found!");
+  }
+
+  const progress = await DSAProgress.findOneAndUpdate(
+    {
+      user: userId,
+      problem: problemId,
+    },
+    {
+      $set: {
+        status,
+        lastAttemptedAt: new Date(),
+        ...(status === "solved" ? { solvedAt: new Date() } : {}),
+      },
+      $inc: {
+        attempts: 1,
+      },
+    },
+    {
+      new: true,
+      upsert: true,
+      runValidators: true,
+    },
+  );
+
+  return progress;
 };
